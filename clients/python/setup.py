bae667669a56450418ac2b2b4e98c2be15d759e1
Zack Kollar <rallokkcaz@gmail.com>
2025-04-02 22:31:59 -0400

    :hammer: Update setup.py for client

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -4,6 +4,8 @@ setup(
     name="bonka-client",
     version="0.1.0",
     packages=find_packages(),
+    license="MIT or Apache-2.0",
+    author="SeedyROM (Zack Kollar)",
     install_requires=[
         "protobuf>=4.21.0",
     ],
